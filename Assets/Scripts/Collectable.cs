d4de3ec1707cbfbce5241849e6deaf27f86b9b87
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2023-05-05 09:54:46 +0100

    Added melon

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collectable : MonoBehaviour
 {
     public float Collect = 0f;
+    public int sceneToGoTo;
 
     void Update()
     {
@@ -17,13 +19,13 @@ public class Collectable : MonoBehaviour
 
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             if (Collect == 1) //Watermelon load next scene
             {
-
+                LoadSpecificScene(sceneToGoTo);
             }
 
             if (Collect == 2)
@@ -31,5 +33,9 @@ public class Collectable : MonoBehaviour
 
             }
         }
+    }
+    void LoadSpecificScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
     }
 }
