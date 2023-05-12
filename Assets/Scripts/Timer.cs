bb58b745be16f17ba98bfdddcc80746459ea6b00
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2023-05-12 11:24:42 +0100

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [Header("component")]
+    public TextMeshProUGUI timerText;
+
+    [Header("Timer Settings")]
+    public float currentTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        timerText.text = currentTime.ToString("0.00");
+    }
+}
